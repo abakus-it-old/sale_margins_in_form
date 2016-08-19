82e589676ffdbc6cdcf67e055f633e41f1a7c8a1
Bernard Delhez <bernarddelhez@gmail.com>
2016-08-19 14:59:39 +0200

    update version to 9

--- __openerp__.py
+++ __openerp__.py
@@ -1,6 +1,6 @@
 {
     'name': "Margins in sale form",
-    'version': '1.0',
+    'version': '9.0.1.0',
     'depends': ['sale',],
     'author': "Valentin THIRION, AbAKUS it-solutions SARL",
     'website': "http://www.abakusitsolutions.eu",
